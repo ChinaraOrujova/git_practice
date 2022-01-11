02eba38296bbedacc66102e5b2e870032a0e8b66
Chinara Orujova <china.orujova@gmail.com>
2022-01-11 11:31:34 -0500

    initial commit for testCase1

--- src/us100/TestCase001.java
+++ src/us100/TestCase001.java
@@ -2,10 +2,6 @@ package us100;
 
 public class TestCase001 {
 
-    //first test case
-    public static void main(String[] args) {
-        System.out.println("User Story");
 
-        //second test case
-    }
+
 }
